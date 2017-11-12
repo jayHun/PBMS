afded372df8e1663f9c2e2549750ec8e33f95d69
Jhun1992 <raresome92@naver.com>
2017-11-12 17:43:46 +0900

    14ch commit

--- src/general/PhoneBookver01.java
+++ src/general/PhoneBookver01.java
@@ -1,4 +1,5 @@
 package general;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class PhoneInfoManager{
@@ -11,19 +12,58 @@ class PhoneInfoManager{
 		System.out.print("선택하세요...\n1. 데이터입력\n2. 데이터검색\n3. 데이터삭제\n4. 프로그램종료\n선택 : ");
 	}
 	
+	public void selType(){
+		System.out.print("1. 일반, 2. 대학, 3. 회사\n선택>> ");
+	}
+	
 	// 데이터 삽입
 	protected boolean insertData(){
+		int type;
+		String name;
+		String phoneNumber;
+		String company;
+		String major;
+		int year;
+		
 		try{
 			if(arraysize==5){
 				return true;
 			}
-			System.out.print("이름 : ");
-			String name=sc.next();
-			System.out.print("전화번호 : ");
-			String pnum=sc.next();
-			System.out.print("생년월일 : ");
-			String birth=sc.next();
-			pinfo[arraysize++]=new PhoneInfo(name, pnum, birth);
+			selType();
+			type=sc.nextInt();
+			switch(type){
+				case 1:
+					System.out.print("이름 : ");
+					name=sc.next();
+					System.out.print("전번 : ");
+					phoneNumber=sc.next();
+					pinfo[arraysize++]=new PhoneInfo(name, phoneNumber);
+					break;
+				case 2:
+					System.out.print("이름 : ");
+					name=sc.next();
+					System.out.print("전번 : ");
+					phoneNumber=sc.next();
+					System.out.print("전공 : ");
+					major=sc.next();
+					System.out.print("나이 : ");
+					year=sc.nextInt();
+					pinfo[arraysize++]=new PhoneUnivInfo(name, phoneNumber, major, year);
+					break;
+				case 3:
+					System.out.print("이름 : ");
+					name=sc.next();
+					System.out.print("전번 : ");
+					phoneNumber=sc.next();
+					System.out.print("회사 : ");
+					company=sc.next();
+					pinfo[arraysize++]=new PhoneCompanyInfo(name, phoneNumber, company);
+					break;
+				default:
+					System.out.println("1~3의 숫자를 입력하세요...");
+					return false;
+			}
+			System.out.println("데이터 입력이 완료되었습니다.\n");
 			return false;
 		}catch(NullPointerException e){
 			e.printStackTrace();
@@ -33,11 +73,16 @@ class PhoneInfoManager{
 	
 	// 데이터 검색
 	protected void searchData(){
+		System.out.println("데이터 검색을 시작합니다...");
 		System.out.print("이름 : ");
 		String sname=sc.next();
 		int idx=findIdx(sname);
-		System.out.println("\n입력된 정보 출력...");
-		pinfo[idx].getAll();
+		if(idx>=0){
+			pinfo[idx].getAll();
+			System.out.println("데이터 검색이 완료되었습니다.\n");
+		}else{
+			System.out.println("해당 데이터가 존재하지 않습니다.\n");
+		}
 	}
 	
 	// 데이터 삭제
@@ -45,49 +90,76 @@ class PhoneInfoManager{
 		System.out.print("이름 : ");
 		String sname=sc.next();
 		int idx = findIdx(sname);
-		for(int i=idx; i<arraysize-1; i++){
-			pinfo[i]=pinfo[i+1];
+		if(idx>=0){
+			for(int i=idx; i<arraysize-1; i++){
+				pinfo[i]=pinfo[i+1];
+			}
+			arraysize--;
+			System.out.println("\n데이터 삭제 완료...");
+		}else{
+			System.out.println("해당 데이터가 존재하지 않습니다.\n");
 		}
-		arraysize--;
-		System.out.println("\n데이터 삭제 완료...");
 	}
 	
 	// 검색, 삭제할 데이터의 인덱스를 반환하는 메서드
 	protected int findIdx(String name){
-		int idx=0;
 		for(int i=0; i<arraysize; i++){
-			if(pinfo[i].name.equals(name)){
-				idx=i;
-				break;
+			if(name.compareTo(pinfo[i].name)==0){
+				return i;
 			}
 		}
-		return idx;
+		return -1;
 	}
 }
 
 class PhoneInfo {
 	protected String name;
 	protected String phoneNumber;
-	protected String birthday;
 	
 	// 모든 정보를 입력할 경우 호출되는 생성자
-	public PhoneInfo(String name, String phoneNumber, String birthday){
-		this.name=name;
-		this.phoneNumber=phoneNumber;
-		this.birthday=birthday;
-	}
-	
-	// 생일을 입력하지 않았을 경우 호출되는 생성자
 	public PhoneInfo(String name, String phoneNumber){
 		this.name=name;
 		this.phoneNumber=phoneNumber;
+				
 	}
 	
 	// 모든 정보를 출력하는 메서드
-	public void getAll(){
-		System.out.println("이름 : " + this.name);
-		System.out.println("전화번호 : " + this.phoneNumber);
-		System.out.println("생일 : " + this.birthday + "\n");
+	protected void getAll(){
+		System.out.println("이름 : " + name);
+		System.out.println("전번 : " + phoneNumber);
+	}
+}
+
+class PhoneUnivInfo extends PhoneInfo{
+	protected String major;
+	protected int year;
+	
+	PhoneUnivInfo(String name, String phoneNumber, String major, int year){
+		super(name, phoneNumber);
+		this.major=major;
+		this.year=year;
+	}
+	
+	protected void getAll(){
+		System.out.println("이름 : " + name);
+		System.out.println("전번 : " + phoneNumber);
+		System.out.println("전공 : " + major);
+		System.out.println("나이 : " + year);
+	}
+}
+
+class PhoneCompanyInfo extends PhoneInfo{
+	protected String company;
+	
+	PhoneCompanyInfo(String name, String phoneNumber, String company){
+		super(name, phoneNumber);
+		this.company=company;
+	}
+	
+	protected void getAll(){
+		System.out.println("이름 : " + name);
+		System.out.println("전번 : " + phoneNumber);
+		System.out.println("회사 : " + company);
 	}
 }
 
@@ -97,26 +169,31 @@ class PhoneBookver01{
 		PhoneInfoManager pim=new PhoneInfoManager();
 		int choice;
 		do{
-			PhoneInfoManager.selMenu();
-			choice = PhoneInfoManager.sc.nextInt();
-			if(choice==1){
-				if(pim.insertData()){
-					System.out.println("전화번호부가 꽉 찼으므로 삭제하십시오.");
+			try{
+				PhoneInfoManager.selMenu();
+				choice = PhoneInfoManager.sc.nextInt();
+				PhoneInfoManager.sc.nextLine();
+				if(choice==1){
+					if(pim.insertData()){
+						System.out.println("Warning : 전화번호부가 꽉 찼으므로 삭제하십시오.\n");
+						continue;
+					}
+				}else if(choice==2){
+					pim.searchData();
 					continue;
+				}else if(choice==3){
+					pim.deleteData();
+					continue;
+				}else if(choice==4){
+					System.out.println("프로그램 종료");
+					PhoneInfoManager.sc.close();
+					return;
+				}else{
+					System.out.println("1~4번 중 하나를 입력하세요\n");
 				}
-				System.out.println("입력 완료\n");
-			}else if(choice==2){
-				pim.searchData();
-				continue;
-			}else if(choice==3){
-				pim.deleteData();
-				continue;
-			}else if(choice==4){
-				System.out.println("프로그램 종료");
-				PhoneInfoManager.sc.close();
-				return;
-			}else{
-				System.out.println("1~4번 중 하나를 입력하세요\n");
+			}catch(InputMismatchException e){
+				PhoneInfoManager.sc = new Scanner(System.in);
+				System.out.println("숫자를 입력하세요.\n");
 			}
 		}while(true);
 	}
